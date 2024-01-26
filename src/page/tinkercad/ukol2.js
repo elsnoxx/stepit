deff4a54baf529d6dae7d88fab13361ad48b2657
elsnoxx <ficek.risa@gmail.com>
2024-01-26 10:04:43 +0100

    Create ukol2.js

---
+++ src/page/tinkercad/ukol2.js
@@ -0,0 +1,12 @@
+// AboutPage.js
+import React from 'react';
+
+const TinkercadUkol2 = () => {
+  return (
+    <div>
+      <h2>Tinkercad Ukol</h2>
+    </div>
+  );
+}
+
+export default TinkercadUkol2;
