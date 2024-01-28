d0c04d1fd57b96de34d7acc4d8d23d488d34ec91
elsnoxx <ficek.risa@gmail.com>
2024-01-28 10:17:50 +0100

    vytvoreni stranek pro prezentaci

---
+++ src/page/general/prezentace.js
@@ -0,0 +1,13 @@
+// AboutPage.js
+import React from 'react';
+
+const EndPresentation = () => {
+  return (
+    <div>
+      <h2>Page about end presentation</h2>
+      <p>This is the about page content.</p>
+    </div>
+  );
+}
+
+export default EndPresentation;
