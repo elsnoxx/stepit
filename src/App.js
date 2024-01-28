6f251dda82fed46789561201c5f9d336c84b783a
elsnoxx <ficek.risa@gmail.com>
2024-01-28 10:16:05 +0100

    zlepseni nazvu cest

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from './page/footer';
 import Homepage from './page/homepage'
 import NoPage from "./page/nopage";
 
-
+// Audacity
 import Audacity from './page/audacity/audacity'
 import AudacityLekce1 from './page/audacity/audacity1';
 import AudacityLekce2 from './page/audacity/audacity2';
@@ -15,6 +15,7 @@ import EnvAudacity from './page/audacity/env';
 import AudacityProjekt from './page/audacity/projekt';
 import AudacityHodnoceni from './page/audacity/hodnoceni';
 
+// TinkerCAD
 import Tinkercad from './page/tinkercad/tinkercad'
 import TinkercadLekce1 from './page/tinkercad/tinkercad1';
 import TinkercadLekce2 from './page/tinkercad/tinkercad2';
@@ -28,13 +29,17 @@ import TinkercadSachy from './page/tinkercad/sachy';
 import TinkercadHodnoceni from './page/tinkercad/hodnoceni';
 import TinkercadBonusukoly from './page/tinkercad/bonusukoly';
 
+// General pages
+import EndPresentation from './page/general/prezentace';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <TopNav />
       <Routes>
-        <Route path="/" element={<TopNav />}>
+        <Route>
           <Route index element={<Homepage />} />
+          {/* Audacity pages */}
           <Route path="audacity" element={<Audacity />} />
           <Route path="audacity/audacity1" element={<AudacityLekce1 />} />
           <Route path="audacity/audacity2" element={<AudacityLekce2 />} />
@@ -43,18 +48,21 @@ export default function App() {
           <Route path="audacity/env" element={<EnvAudacity />} />
           <Route path="audacity/projekt" element={<AudacityProjekt />} />
           <Route path="audacity/hodnoceni" element={<AudacityHodnoceni />} />
+          {/* TinkerCADc pages */}
           <Route path="tinkercad" element={<Tinkercad />} />
-          <Route path="tinkercad/tinkercad1" element={<TinkercadLekce1 />} />
-          <Route path="tinkercad/tinkercad2" element={<TinkercadLekce2 />} />
-          <Route path="tinkercad/tinkercad3" element={<TinkercadLekce3 />} />
-          <Route path="tinkercad/tinkercad4" element={<TinkercadLekce4 />} />
-          <Route path="tinkercad/tinkercad5" element={<TinkercadLekce5 />} />
+          <Route path="tinkercad/lekce1" element={<TinkercadLekce1 />} />
+          <Route path="tinkercad/lekce2" element={<TinkercadLekce2 />} />
+          <Route path="tinkercad/lekce3" element={<TinkercadLekce3 />} />
+          <Route path="tinkercad/lekce4" element={<TinkercadLekce4 />} />
+          <Route path="tinkercad/lekce5" element={<TinkercadLekce5 />} />
           <Route path="tinkercad/ukol" element={<TinkercadUkol />} />
           <Route path="tinkercad/login" element={<TinkercadLogin />} />
           <Route path="tinkercad/projekt" element={<TinkercadProjekt />} />
           <Route path="tinkercad/sachy" element={<TinkercadSachy />} />
           <Route path="tinkercad/hodnoceni" element={<TinkercadHodnoceni />} />
           <Route path="tinkercad/bonusukoly" element={<TinkercadBonusukoly />} />
+          {/* General pages */}
+          <Route path="prezentace" element={<EndPresentation />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
