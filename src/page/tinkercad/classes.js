61842591fafc4c6fe10e1773859807a75fb1ed47
elsnoxx <ficek.risa@gmail.com>
2024-01-29 13:43:29 +0100

    vytvoreni komponenty pro odkazy do trid

---
+++ src/page/tinkercad/classes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ClassLogin = () => {
+  return (
+    <div >
+      <h5>
+        <strong>Link pro přihlášení do naší TinkerCad třídy:</strong>
+      </h5>
+      <ul>
+        <li><a href="https://www.tinkercad.com/joinclassName/3QY3173UJ">JA-MON-O-3-23</a></li>
+        <li><a href="https://www.tinkercad.com/joinclassName/RKEWPZBPC">JA-TH-O-10-23</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default ClassLogin;
